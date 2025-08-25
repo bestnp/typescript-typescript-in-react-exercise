5bfc63551326e26299c655ee9a37772e7d264fc6
PTAE43 <kritsada1696@gmail.com>
2025-08-25 14:26:02 +0700

    Done: Ex.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return (
+    <Greeting name="TAE" age={31} />
+  )
 }
 
 export default App;
